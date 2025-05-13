5f019de853279f8c16a96bd60d683a5490084e0e
Bryan Siegel <bryansiegel@gmail.com>
2025-05-13 11:29:09 -0700

    initial commit

---
+++ includes/head.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<body>
+<head>
+    <!-- meta tag -->
+    <meta charset="utf-8">
+    <title>Back to School</title>
+    <meta name="description" content="2024-2025 Clark County School District Back to School and Legal Guides">
+    <!-- responsive tag -->
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content="Back to School"/>
+    <meta property="og:image" content="https://ccsd.net/district/backtoschool/assets/images/breadcrumbs/register-bts.jpg"/>
+    <meta property="og:description" content='2024-2025 Clark County School District Back to School and Legal Guides'/>
+    <meta property="og:url" content="https://ccsd.net/district/backtoschool/index.html"/>
+    <meta property="og:image:width" content="770" />
+    <meta property="og:image:height" content="426" />
+    <!-- favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/fav.png">
+    <!-- Bootstrap v5.0.2 css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <!-- font-awesome css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+    <!-- animate css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+    <!-- owl.carousel css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/owl.carousel.css">
+    <!-- slick css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/slick.css">
+    <!-- off canvas css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/off-canvas.css">
+    <!-- linea-font css -->
+    <link rel="stylesheet" type="text/css" href="assets/fonts/linea-fonts.css">
+    <!-- flaticon css  -->
+    <link rel="stylesheet" type="text/css" href="assets/fonts/flaticon.css">
+    <!-- magnific popup css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/magnific-popup.css">
+    <!-- Main Menu css -->
+    <link rel="stylesheet" href="assets/css/rsmenu-main.css">
+    <!-- spacing css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/rs-spacing.css">
+    <!-- style css -->
+    <link rel="stylesheet" type="text/css" href="style.css"> <!-- This stylesheet dynamically changed from style.less -->
+    <!-- responsive css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2X42CPJS5R"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-2X42CPJS5R');
+    </script>
+</head>
