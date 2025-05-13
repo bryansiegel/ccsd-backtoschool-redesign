71c2d7bfde79820ffaa3bed406bc69905a7ad7c7
Bryan Siegel <bryansiegel@gmail.com>
2025-05-13 13:27:34 -0700

    refactored

---
+++ includes/about/accordion.php
@@ -0,0 +1,140 @@
+<div class="col-lg-8 md-mb-50">
+    <!-- Intro Info Tabs-->
+    <div class="intro-info-tabs">
+        <div class="accordion" id="accordionPanelsStayOpenExample">
+
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="panelsStayOpen-heading1">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#panelsStayOpen-collapse1" aria-expanded="true"
+                            aria-controls="panelsStayOpen-collapse1">
+                        Registration
+                    </button>
+                </h2>
+                <div id="panelsStayOpen-collapse1" class="accordion-collapse collapse show"
+                     aria-labelledby="panelsStayOpen-heading1">
+                    <div class="accordion-body">
+                        <p><em>Registration Began April 15, 2024<br>
+                                Classes Begin August 12, 2024</em></p>
+
+                        <p>Parents/Guardians must register all new and returning students to the Clark
+                            County School District.</p>
+
+                        <p>Registration is done through the CCSD online registration system.
+                            Parents/Guardians of currently enrolled students may complete online
+                            registration using their Infinite Campus account. New families to CCSD may
+                            register online at <a href="https://ccsd.net/parents/enrollment"
+                                                  target="_blank">register.ccsd.net</a> or visit their
+                            zoned school. After completing the online registration, parents/guardians
+                            must bring the required documents to their child’s zoned school to complete
+                            the registration process. Parents/Guardians should contact their zoned
+                            school for questions regarding registration.</p>
+
+                        <p>Visit <a href="https://ccsd.net/schools/zoning/" target="_blank">ccsd.net/zoning</a>
+                            or call Demographics, Zoning, and GIS at <a href="tel:(702)-799-6430">702-799-6430</a>
+                            for zoning information.</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="panelsStayOpen-heading2">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#panelsStayOpen-collapse2" aria-expanded="false"
+                            aria-controls="panelsStayOpen-collapse2">
+                        Age and Entrance Requirements
+                    </button>
+                </h2>
+                <div id="panelsStayOpen-collapse2" class="accordion-collapse collapse"
+                     aria-labelledby="panelsStayOpen-heading2">
+                    <div class="accordion-body">
+                        <p>Children must meet the following minimum age and entrance requirements on or
+                            before August 1 to be admitted to kindergarten, first, or second grade
+                            (Nevada Revised Statutes [NRS] 392.040):</p>
+                        <p><strong>✔ Kindergarten Students</strong> must have attained the age of five
+                            years old by August 1 to attend kindergarten.</p>
+
+                        <p><strong>✔ Grade 1 Students</strong> must have attained the age of six years
+                            old by August 1 and demonstrate proof of completing a minimum of six weeks
+                            of kindergarten at a public, charter, or private school. The kindergarten
+                            requirement cannot be met via online or a homeschool program. (A student who
+                            is enrolled in and successfully completes kindergarten may be promoted to
+                            the next grade level regardless of their age during the 2024–2025 school
+                            year.)
+                        <p align="center">OR</p>
+                        <p><strong>✔</strong> Demonstrate proof of completing kindergarten and promotion
+                            to first grade while being a resident of another state/country in compliance
+                            with the age and entrance requirements of that state/country. Proof must be
+                            verified through a report card issued by the last out-of-district public,
+                            charter or private kindergarten.</p>
+                        <p>Children who have attained the age of six who do not meet the entrance
+                            requirements of Grade 1 will be placed in kindergarten. </p>
+
+                        <p><strong>✔ Grade 2 Students</strong> must have attained the age of seven years
+                            old and demonstrate proof of completing kindergarten and first grade at a
+                            public, charter, or private school. (A student who is enrolled in and
+                            successfully completes first grade may be promoted to the next grade level
+                            regardless of their age during the 2024–2025 school year.)</p>
+
+                        <p>Children who have attained the age of seven who do not meet the entrance
+                            requirements of Grade 2 will be assessed to determine if the student is
+                            developmentally ready for first or second grade.</p>
+
+                        <p>Children in Nevada between the ages of six and 18 must be enrolled in and
+                            attend school for the entire time school is in session, pursuant to NRS
+                            392.040.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="panelsStayOpen-heading3">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#panelsStayOpen-collapse3" aria-expanded="false"
+                            aria-controls="panelsStayOpen-collapse3">
+                        Registration Required Documents
+                    </button>
+                </h2>
+                <div id="panelsStayOpen-collapse3" class="accordion-collapse collapse"
+                     aria-labelledby="panelsStayOpen-heading3">
+                    <div class="accordion-body">
+
+                        <p><strong>1. Proof of Address</strong>: One form of proof of address is
+                            required. A recent utility bill (telephone and cable bills are not
+                            acceptable), current mortgage statement, rental receipt, residential lease
+                            agreement, or sales contract are acceptable forms of proof of address. </p>
+
+                        <p><strong>2. Parent/Guardian Personal Identification</strong>: A driver’s
+                            license, picture identification, and/or passport are acceptable forms of
+                            personal identification. Permanent and temporary guardians should be
+                            prepared to provide proof of guardianship at the time of registration. </p>
+
+                        <p><strong>3. Proof of Child’s Identity</strong>: An original birth certificate,
+                            passport, or certified birth card issued by a health district are acceptable
+                            documents for proof of a child's identity. </p>
+
+                        <p><strong>4. Immunizations: Nevada law requires public school students to be
+                                immunized. Parents/Guardians also must provide an appropriate valid
+                                certificate with the documentation. Immunization requirements are
+                                outlined in NRS 392.435.</strong></p>
+
+                        <p>Unless excused because of religious belief or medical condition, a child may
+                            not be enrolled in a public school within Nevada unless the child has been
+                            immunized and has received the proper boosters for that immunization or is
+                            complying with the schedules established pursuant to NRS 439.550.</p>
+
+                        <p>For detailed information concerning immunization requirements for children
+                            attending CCSD schools, visit <a
+                                href="https://ccsd.net/parents/enrollment#proofImm" target="_blank">Student
+                                Enrollment Information</a>.</p>
+
+                        For assistance with obtaining immunizations, parents/guardians may contact the
+                        Southern Nevada Health District at <a href="tel:(702)-759-0850">702-759-0850</a>.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ includes/about/quicklinks.php
@@ -0,0 +1,28 @@
+<div class="video-column col-lg-4">
+    <div class="inner-column">
+        <div class="course-features-info" style="margin-top:-1px;">
+            <h3>Quick Links</h3>
+            <ul>
+                <li class="lectures-feature">
+                                    <span class="label"><a href="https://ccsd.net/parents/enrollment#proofImm"
+                                                           target="_blank">Immunization Requirements</a></span>
+                </li>
+
+                <li class="quizzes-feature">
+                    <span class="label"><a href="https://ccsd.net/parents/enrollment" target="_blank">Enrollment Information</a></span>
+                </li>
+
+                <li class="duration-feature">
+                    <span class="label"><a href="https://ccsd.net/schools/zoning/" target="_blank">Zoning Information</a></span>
+                </li>
+
+
+            </ul>
+        </div>
+
+
+        <?php include('includes/side-bar.php'); ?>
+
+
+    </div>
+</div>
